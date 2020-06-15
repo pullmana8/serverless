5fd09a2db11394f781d62906bdc6732fcd7937ef
Antonette Caldwell <pullmana8@gmail.com>
2020-06-15 09:41:38 -0500

    Moving codes around

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Router, Link } from 'react-router-dom';
-import Auth from './auth/Auth';
+import { Router, Link, Switch, Route } from 'react-router-dom';
 import { Menu, Grid, Segment } from 'semantic-ui-react';
+
+import Auth from './auth/Auth';
+import { NotFound } from 'components/NotFound';
 
 export interface AppProps { }
 export interface AppProps {
@@ -29,17 +31,21 @@ export default class App extends Component<AppProps, AppState> {
 
   render() {
     return (
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Grid container stackable verticalAlign="middle">
-          <Grid.Row>
-            <Grid.Column width={16}>
-              <Router history={this.props.history}>
-                {this.generateMenu()}
-              </Router>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <div>
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign="middle">
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Router history={this.props.history}>
+                  {this.generateMenu()}
+
+                  {this.generateCurrentPage()}
+                </Router>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </div>
     )
   }
 
@@ -58,14 +64,44 @@ export default class App extends Component<AppProps, AppState> {
   logInLogOutButton() {
     if (this.props.auth.isAuthenticated()) {
       return (
-        <Menu.Item id="logout" onClick={this.handleLogout}>
+        <Menu.Item name="logout" onClick={this.handleLogout}>
           Log Out
         </Menu.Item>
       )
     } else {
-      <Menu.Item id="login" onClick={this.handleLogin}>
-        Log In
-      </Menu.Item>
+      return (
+        <Menu.Item name="login" onClick={this.handleLogin}>
+          Log In
+        </Menu.Item>
+      )
     }
+  }
+
+  generateCurrentPage() {
+    if (!this.props.auth.isAuthenticated()) {
+      return <LogIn auth={this.props.auth} />
+    }
+
+    return (
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={props => {
+            return <Todos {...props} auth={this.props.auth} />
+          }}
+        />
+
+        <Route
+          path="/todos/:todoId/edit"
+          exact
+          render={props => {
+            return <EditTodo {...props} auth={this.props.auth} />
+          }}
+        />
+
+        <Route component={NotFound} />
+      </Switch>
+    )
   }
 }
